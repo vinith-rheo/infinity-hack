1b9cb741f7f2e6de14d66cd4fe5a5ef552a9b159
Kaustubh-rheo <kaustubh.u@rheo.ai>
2025-06-06 09:08:26 +0530

    initial commit

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,8 +2,9 @@ import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>{`Infinity Hack : Team (Undecided yet :'))`}</Button>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-black">
+      <h1 className="text-3xl font-bold text-white">Welcome to Infinity Hack</h1>
+        <Button className="bg-cyan-500 m-4">{`Infinity Hack : Team (Name Undecided yet :'))`}</Button>
     </div>
   )
 }
