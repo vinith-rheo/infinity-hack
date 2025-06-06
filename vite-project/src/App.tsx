a8bce824419f1e8bd895f251a938bc840313a5a9
Kaustubh-rheo <kaustubh.u@rheo.ai>
2025-06-06 09:03:57 +0530

    Checking git access

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+        <Button>{`Infinity Hack : Team (Undecided yet :'))`}</Button>
     </div>
   )
 }
