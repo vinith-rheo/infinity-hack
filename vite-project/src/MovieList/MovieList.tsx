4cd29a394686e9ce8c6aba02bd85fcc3a973b275
Kaustubh-rheo <kaustubh.u@rheo.ai>
2025-06-10 22:16:28 +0530

    Fixed SignIn / Out issue

--- vite-project/src/MovieList/MovieList.tsx
+++ vite-project/src/MovieList/MovieList.tsx
@@ -3,6 +3,7 @@ import movieData from "../data.json";
 import { Input } from "@/components/ui/input";
 import { useEffect } from "react";
 import { getMovies } from "@/services";
+import { UserButton } from "@clerk/clerk-react";
 export default function MovieList() {
   useEffect(() => {
     getMovies().then((data) => {
@@ -11,7 +12,10 @@ export default function MovieList() {
   }, []);
   return (
     <>
+  <div className="flex justify-between">
       <Input className="w-1/2 mb-3 h-12" placeholder="Search for movies" />
+       <UserButton afterSignOutUrl={'/'}/>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {movieData.map((movie) => {
           return (
