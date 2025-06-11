a231ae4f7b52082ba325d9a743b674fb39bf0859
vinithmoduguru <vinith1142@gmail.com>
2025-06-12 01:06:10 +0530

    Refactor movie data handling: Introduced Movie interface, updated getMovies function, and modified MovieCard and MovieList components to utilize poster_url and manage loading state.

--- vite-project/src/MovieList/MovieList.tsx
+++ vite-project/src/MovieList/MovieList.tsx
@@ -1,33 +1,62 @@
 import MovieCard from "@/MovieCard/MovieCard";
-import movieData from "../data.json";
 import { Input } from "@/components/ui/input";
-import { useEffect } from "react";
-import { getMovies } from "@/services";
+import { useEffect, useState } from "react";
+import { getMovies, type Movie } from "@/services";
 import { UserButton } from "@clerk/clerk-react";
+import { Loader2 } from "lucide-react";
 export default function MovieList() {
+  const [moviesLoading, setMoviesLoading] = useState(false);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const fetchMovies = async () => {
+    setMoviesLoading(true);
+    const data = await getMovies();
+    setMovies(data);
+    setMoviesLoading(false);
+  };
+
   useEffect(() => {
-    getMovies().then((data) => {
-      console.log(data);
-    });
+    fetchMovies();
   }, []);
   return (
-    <div className='w-full min-h-screen bg-[#000000]'>
-  <div className="flex justify-between w-full pt-2 bg-[#000000]">
-      <Input className="w-1/2 mb-3 h-12 bg-[#B6B09F]" placeholder="Search for movies" />
-      <span className="p-2">
-         <UserButton afterSignOutUrl={'/'}/>
-      </span>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {movieData.map((movie) => {
-          return (
-            <MovieCard
-              key={movie.id}
-              movie={movie}
+    <>
+      {moviesLoading ? (
+        <div className="flex justify-center items-center h-screen">
+          <Loader2 className="w-10 h-10 animate-spin" />
+        </div>
+      ) : (
+        <div className="w-full min-h-screen bg-[#000000]">
+          <div className="flex justify-between w-full pt-2 bg-[#000000]">
+            <Input
+              className="w-1/2 mb-3 h-12 bg-[#B6B09F]"
+              placeholder="Search for movies"
             />
-          );
-        })}
-      </div>
-    </div>
+            <span className="p-2">
+              <UserButton afterSignOutUrl={"/"} />
+            </span>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {movies.map((movie) => {
+              return (
+                <MovieCard
+                  key={movie.id}
+                  movie={{
+                    genres: [],
+                    title: movie.title,
+                    tagline: movie.tagline,
+                    overview: movie.overview,
+                    poster_url: movie.poster_url,
+                    release_date: String(movie.release_date),
+                    vote_average: movie.vote_average,
+                    runtime: movie.runtime,
+                    homepage: movie.homepage,
+                  }}
+                />
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- vite-project/src/services.tsx
+++ vite-project/src/services.tsx
@@ -1,4 +1,32 @@
-export async function getMovies() {
+export interface Movie {
+  _id: string;
+  adult: string;
+  belongs_to_collection: string;
+  budget: number;
+  genres: string;
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  poster_url: string;
+  production_companies: string;
+  production_countries: string;
+  release_date: Date;
+  revenue: number;
+  runtime: number;
+  spoken_languages: string;
+  status: string;
+  tagline: string;
+  title: string;
+  video: string;
+  vote_average: number;
+  vote_count: number;
+}
+export async function getMovies(): Promise<Movie[]> {
   const response = await fetch("/api/movies/list");
   const data = await response.json();
   return data.movies;
